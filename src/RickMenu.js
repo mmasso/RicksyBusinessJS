33c600d543f2148a8f8fb7668f3929a50ae7d25b
mmasso <mmasso@cifpfbmoll.eu>
2020-12-08 12:19:14 +0100

    feat(RickMenu.js): Implementacion de rickmenu

---
+++ src/RickMenu.js
@@ -0,0 +1,18 @@
+function RickMenu(stock, cost) {
+    this.stock = stock;
+    this.cost = cost;
+}
+
+RickMenu.prototype.dispatch = function(creditCard) {
+    //check si hay stock y el objeto se ha pagado o no, si las dos son true, se resta stock
+    if (this.isDispatchable() && creditCard.pay(this.price)) {
+        this.stock -= 1;
+    }
+}
+
+PackExpender.prototype.isDispatchable = function() {
+    //check stock
+    return this.stock > 0
+}
+
+module.exports = RickMenu;
